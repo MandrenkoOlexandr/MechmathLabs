da691a29bba6309809c49617442e386016918b1b
MandrenkoOlexandr <113349907+MandrenkoOlexandr@users.noreply.github.com>
2022-11-12 20:32:56 +0200

    Lab 7-8

---
+++ aud_lab7-8_task6_2.cpp
@@ -0,0 +1,20 @@
+// Auditor lab 7-8 task 6.2
+// Mandrenko Olexandr
+
+#include <stdio.h>
+void task6_2()
+{
+    int n;
+    unsigned m;
+    printf("n = ");
+    scanf("%d", &n);
+    printf("k = ");
+    scanf("%u", &m);
+    int res = n | (1 << m);
+    printf("r = %d", res);
+}
+
+int main()
+{
+    task6_2();
+}
---
+++ aud_lab7-8_task6_3.cpp
@@ -0,0 +1,25 @@
+// Auditor lab 7-8 task 6.3
+// Mandrenko Olexandr
+
+#include <stdio.h>
+#include <stdint.h>
+void task6_3()
+{
+    uint64_t m;
+    uint8_t j;
+
+    printf("m=");
+    scanf("%llu", &m);
+    
+    printf("j=");
+    scanf("%hhu", &j);
+
+    m = m & ~(1<<(j-1));
+    
+    printf("m=%llu", m);
+}
+
+int main()
+{
+    task6_3();
+}
